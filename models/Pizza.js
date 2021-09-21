63febd2adbb604c7ceaf1ac58887959e02794cab
anthonycc <anthonyccardozo@gmail.com>
2021-09-21 12:26:35 -0400

    created comment model, testing comment routes

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,25 +1,46 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
-        type: String
+      type: String,
     },
     createdBy: {
-        type: String
+      type: String,
     },
     createdAt: {
-        type: String,
-        default: Date.now
+      type: String,
+      default: Date.now,
     },
     size: {
-        type: String,
-        default: 'Large'
+      type: String,
+      default: "Large",
     },
-    toppings: [] // could also say 'Array' instead of []
+    toppings: [
+      // could also say 'Array' instead of []
+    ],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of comments and replies upon retrieval
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
 });
 
 // create the Pizza model using the PizzaSchema
-const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
 // export the Pizza model
 module.exports = Pizza;
